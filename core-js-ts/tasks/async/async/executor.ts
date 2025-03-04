31c38a2112fc43b33ea122660087f59d6e94abc6
Mikhail_Stankevich <Mikhail_Stankevich@epam.com>
2025-03-04 09:33:06 +0100

    feat: add executor and simple server for async task handling

---
+++ core-js-ts/tasks/async/async/executor.ts
@@ -0,0 +1,36 @@
+import { SimpleServer } from "./server";
+
+type DataItem = { id: string; value: number };
+
+class Executor {
+  private server: SimpleServer | null = null;
+  private readonly data: DataItem[] = Array.from(Array(3000).keys()).map((item) => ({
+    id: crypto.randomUUID(),
+    value: item,
+  }));
+
+  constructor(
+    private readonly task: (data: DataItem[]) => Promise<void>,
+    serverPort: number = 3000,
+  ) {
+    this.server = new SimpleServer(serverPort);
+  }
+
+  public async start() {
+    try {
+      await this.server?.startServer();
+      await this.runTaskWithTimeout(this.data);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(`❌ ${error.toString()}`);
+      }
+      console.error(error);
+    } finally {
+      this.server?.stopServer();
+    }
+  }
+
+  public async runTaskWithTimeout(data: DataItem[]) {
+    return this.task(data)
+  }
+}
